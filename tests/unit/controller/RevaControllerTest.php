cbfa0ef9ee010cdf295162b05daa05a1f4237c84
Triantafullenia-Doumani <Triantafulleniad@gmail.com>
2021-09-20 12:02:23 +0200

    testAuthenticate

--- tests/unit/controller/RevaControllerTest.php
+++ tests/unit/controller/RevaControllerTest.php
@@ -11,36 +11,58 @@ use OCA\ScienceMesh\Service\UserService;
 
 
 class RevaControllerTest extends PHPUnit_Framework_TestCase {
+
 	private $controller;
-	private $userId = 'john';
+	private $appName = 'sciencemesh';
+	private $rootFolder;
+	private $request;
+	private $session;
+	private $userManager;
+	private $urlGenerator;
+	private $userId = 'einstein';
+	private $config;
+	private $userService;
+	private $trashManager;
 
 	public function setUp() {
-		
+
 		// arg names: $AppName, IRootFolder $rootFolder, IRequest $request, ISession $session,
 		// IUserManager $userManager, IURLGenerator $urlGenerator, $userId, IConfig $config,
-		// \OCA\ScienceMesh\Service\UserService $UserService, ITrashManager $trashManager) 
-	  $appName = 'sciencemesh';
-		$rootFolder = $this->getMockBuilder('OCP\Files\IRootFolder')->getMock();
-		$request = $this->getMockBuilder('OCP\IRequest')->getMock();
-		$session =  $this->getMockBuilder('OCP\ISession')->getMock();
-		$userManager =  $this->getMockBuilder('OCP\IUserManager')->getMock();
-		$urlGenerator =  $this->getMockBuilder('OCP\IURLGenerator')->getMock();
-		$config =  $this->getMockBuilder('OCP\IConfig')->getMock();
-		$userService  =  new UserService($session);
-		$trashManager =  $this->getMockBuilder('OCA\Files_Trashbin\Trash\ITrashManager')->getMock();
-		
-		$this->controller = new RevaController(
-			$appName, $rootFolder, $request, $session,
-			$userManager, $urlGenerator, $this->userId, $config,
-			$userService, $trashManager
-		);
+		// \OCA\ScienceMesh\Service\UserService $UserService, ITrashManager $trashManager)
+
+		$this->rootFolder = $this->getMockBuilder('OCP\Files\IRootFolder')->getMock();
+		$this->request = $this->getMockBuilder('OCP\IRequest')->getMock();
+		$this->session =  $this->getMockBuilder('OCP\ISession')->getMock();
+		$this->userManager =  $this->getMockBuilder('OCP\IUserManager')->getMock();
+		$this->urlGenerator =  $this->getMockBuilder('OCP\IURLGenerator')->getMock();
+		$this->config =  $this->getMockBuilder('OCP\IConfig')->getMock();
+		$this->userService  =  new UserService($this->session);
+
+		$this->trashManager =  $this->getMockBuilder('OCA\Files_Trashbin\Trash\ITrashManager')->getMock();
+
 	}
 
-	public function testGetFileSystem() {
-		$result = $this->controller->Authenticate(5);
-
-		$this->assertEquals('index', $result->getTemplateName());
-		$this->assertTrue($result instanceof TemplateResponse);
+	public function testAuthenticateOK() {
+		$this->request->method('getParam')
+						 ->willReturn('relativity');
+		$this->controller = new RevaController(
+			$this->appName, $this->rootFolder, $this->request, $this->session,
+			$this->userManager, $this->urlGenerator, $this->userId, $this->config,
+			$this->userService, $this->trashManager
+		);
+		$result = $this->controller->Authenticate($this->userId);
+		$this->assertEquals($result->getData(), "Logged in");
+	}
+	public function testAuthenticateWrong() {
+		$this->request->method('getParam')
+						 ->willReturn('wrong');
+		$this->controller = new RevaController(
+			$this->appName, $this->rootFolder, $this->request, $this->session,
+			$this->userManager, $this->urlGenerator, $this->userId, $this->config,
+			$this->userService, $this->trashManager
+		);
+		$result = $this->controller->Authenticate($this->userId);
+		$this->assertEquals($result->getData(), "Username / password not recognized");
 	}
 
 }
