f599e343add1292e6815521f38f1301b14c11e0b
Michiel de Jong <michiel@unhosted.org>
2021-09-23 09:19:12 +0200

    uncomment and whitespace

--- tests/unit/controller/RevaControllerTest.php
+++ tests/unit/controller/RevaControllerTest.php
@@ -259,40 +259,41 @@ class RevaControllerTest extends PHPUnit_Framework_TestCase {
 		$this->assertEquals($result->getData(),$response);
 	}
 
-	// public function testListFolder(){
-	// 	$testFile = $this->getMockBuilder("OCP\Files\File")->getMock();
-	// 	$this->request->method("getParam")->willReturn("/test.json");
-	// 	$folderContents = [
-	// 	"data"=>[],
-	// 	"headers"=>[
-	// 		"Cache-Control"=>"no-cache, no-store, must-revalidate",
-	// 		"Content-Type"=>"application/json; charset=utf-8"
-	// 	],
-	// 	"cookies"=>[],
-	// 	"status"=>123,
-	// 	"lastModified"=>NULL,
-	// 	"ETag"=>NULL,
-	// 	"contentSecurityPolicy"=>NULL,
-	// 	"featurePolicy"=>NULL,
-	// 	"throttled"=>false,
-	// 	"throttleMetadata"=>[]
-	// 	];
-	// 	$this->sciencemeshFolder->method("get")
-	// 		->with($this->equalTo("/test.json"))
-	// 		->willReturn($testFile);
-	// 	$this->sciencemeshFolder->method("getDirectoryListing")
-	// 		->with($this->equalTo("/test.json"))
-	// 		->willReturn($folderContents);
-	// 	$controller = new RevaController(
-	// 		$this->appName, $this->rootFolder, $this->request, $this->session,
-	// 		$this->userManager, $this->urlGenerator, $this->userId, $this->config,
-	// 		$this->userService, $this->trashManager
-	// 	);
-	//
-	// 	$result = $controller->ListFolder($this->userId);
-	// 	var_dump($result);
-	// 	//$this->assertEquals($result->getData(),$folderContents);
-	// }
+	public function testListFolder(){
+		$testFile = $this->getMockBuilder("OCP\Files\File")->getMock();
+		$this->request->method("getParam")->willReturn("/test.json");
+		$folderContents = [
+		"data"=>[],
+		"headers"=>[
+			"Cache-Control"=>"no-cache, no-store, must-revalidate",
+			"Content-Type"=>"application/json; charset=utf-8"
+		],
+		"cookies"=>[],
+		"status"=>123,
+		"lastModified"=>NULL,
+		"ETag"=>NULL,
+		"contentSecurityPolicy"=>NULL,
+		"featurePolicy"=>NULL,
+		"throttled"=>false,
+		"throttleMetadata"=>[]
+		];
+		$this->sciencemeshFolder->method("get")
+			->with($this->equalTo("/test.json"))
+			->willReturn($testFile);
+		$this->sciencemeshFolder->method("getDirectoryListing")
+			->with($this->equalTo("/test.json"))
+			->willReturn($folderContents);
+		$controller = new RevaController(
+			$this->appName, $this->rootFolder, $this->request, $this->session,
+			$this->userManager, $this->urlGenerator, $this->userId, $this->config,
+			$this->userService, $this->trashManager
+		);
+	
+		$result = $controller->ListFolder($this->userId);
+		var_dump($result);
+		//$this->assertEquals($result->getData(),$folderContents);
+	}
+
 	public function testListGrants(){
 		$this->request->method("getParam")->willReturn("/test.json");
 		$controller = new RevaController(
@@ -304,48 +305,47 @@ class RevaControllerTest extends PHPUnit_Framework_TestCase {
 		$this->assertEquals($result->getData(),"Not implemented");
 	}
 
+	public function testListRecycle(){
 
-		public function testListRecycle(){
-
-			$data =[[
-				"mimetype"=>"application/json",
-				"path"=>"/file1.json",
-				"size"=>1234,
-				"basename"=>"file1.json",
-				"timestamp"=>1234567890,
-				'deleted'=>1234567890,
-				"type"=>"file",
-				"visibility"=>"public"
-			]];
-			$user =  $this->getMockBuilder("OCP\IUser")->getMock();
-			$this->userManager->method("get")->willReturn($user);
-			$item1 = $this->getMockBuilder("OCA\Files_Trashbin\Trash\ITrashItem")->getMock();
-			$item1->method("getOriginalLocation")
-				->willReturn("sciencemesh/file1.json");
-			$item2 = $this->getMockBuilder("OCA\Files_Trashbin\Trash\ITrashItem")->getMock();
-			$item2->method("getOriginalLocation")
-				->willReturn("somethingElse/file2.json");
-			$trashItems = [
-				$item1,
-				$item2
-			];
-			$this->trashManager->method("listTrashRoot")
-				->willReturn($trashItems);
-			$item1->method("getMimetype")->willReturn("application/json");
-			$item1->method("getPath")->willReturn("file1.json");
-			$item1->method("getSize")->willReturn(1234);
-			$item1->method("getMTime")->willReturn(1234567890); // should this be seconds or milliseconds?
-			$item1->method("getDeletedTime")->willReturn(1234567890);
-			$item1->method("getType")->willReturn(\OCP\Files\FileInfo::TYPE_FILE);
-			$controller = new RevaController(
-				$this->appName, $this->rootFolder, $this->request, $this->session,
-				$this->userManager, $this->urlGenerator, $this->userId, $this->config,
-				$this->userService, $this->trashManager
-			);
-			$result = $controller->ListRecycle($this->userId);
-		#	var_dump($result);
-			$this->assertEquals($result->getData(),$data);
-		}
+		$data =[[
+			"mimetype"=>"application/json",
+			"path"=>"/file1.json",
+			"size"=>1234,
+			"basename"=>"file1.json",
+			"timestamp"=>1234567890,
+			'deleted'=>1234567890,
+			"type"=>"file",
+			"visibility"=>"public"
+		]];
+		$user =  $this->getMockBuilder("OCP\IUser")->getMock();
+		$this->userManager->method("get")->willReturn($user);
+		$item1 = $this->getMockBuilder("OCA\Files_Trashbin\Trash\ITrashItem")->getMock();
+		$item1->method("getOriginalLocation")
+			->willReturn("sciencemesh/file1.json");
+		$item2 = $this->getMockBuilder("OCA\Files_Trashbin\Trash\ITrashItem")->getMock();
+		$item2->method("getOriginalLocation")
+			->willReturn("somethingElse/file2.json");
+		$trashItems = [
+			$item1,
+			$item2
+		];
+		$this->trashManager->method("listTrashRoot")
+			->willReturn($trashItems);
+		$item1->method("getMimetype")->willReturn("application/json");
+		$item1->method("getPath")->willReturn("file1.json");
+		$item1->method("getSize")->willReturn(1234);
+		$item1->method("getMTime")->willReturn(1234567890); // should this be seconds or milliseconds?
+		$item1->method("getDeletedTime")->willReturn(1234567890);
+		$item1->method("getType")->willReturn(\OCP\Files\FileInfo::TYPE_FILE);
+		$controller = new RevaController(
+			$this->appName, $this->rootFolder, $this->request, $this->session,
+			$this->userManager, $this->urlGenerator, $this->userId, $this->config,
+			$this->userService, $this->trashManager
+		);
+		$result = $controller->ListRecycle($this->userId);
+	#	var_dump($result);
+		$this->assertEquals($result->getData(),$data);
+	}
 
 	public function testListRevisions(){
 		$this->request->method("getParam")->willReturn("/test.json");
