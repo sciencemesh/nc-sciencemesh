8b5e0c941c0466f99567900c339abc9639e807a9
Your Name <you@example.com>
2021-11-23 09:11:29 +0000

    commented out next steps in crud tests cycle

--- tests/integration/SharesCrudTest.php
+++ tests/integration/SharesCrudTest.php
@@ -29,14 +29,49 @@ function curlPost($apiPath, $params = []) {
  * This test lists shares, creates one, lists them again, removes it, lists them again
  * At least that's the plan :)
  */
-class SharesCruTest extends PHPUnit_Framework_TestCase {
+class SharesCrudTest extends PHPUnit_Framework_TestCase {
 	private $container;
 
 	public function setUp() {
 	}
 
-	public function testCreateHome() {
-    $output = curlPost("storage/CreateHome");
-		$this->assertEquals("\"OK\"", $output);
+	public function testCrudCycleForReceived() {
+    $output = curlPost("ocm/ListReceivedShares", []);
+		$this->assertEquals("[]", $output);
+    // $output = curlPost("ocm/addReceivedShare", [
+		// 	"md" => [
+		// 		"opaque_id" => "fileid-einstein%2Fmy-folder",
+		// 	],
+		// 	"g" => [
+		// 		"grantee" => [
+		// 			"type" => 1,
+		// 		],
+		// 		"Id" => [
+		// 			"UserId" => [
+		// 				"idp" => "cesnet.cz",
+		// 				"opaque_id" => "marie",
+		// 				"type" => 1,
+		// 			],
+		// 		],
+		// 	],
+		// 	"provider_domain" => "cern.ch",
+		// 	"resource_type" => "file",
+		// 	"provider_id" => 2,
+		// 	"owner_display_name" => "Albert Einstein",
+		// 	"protocol" => [
+		// 		"name" => "webdav",
+		// 		"options" => [
+		// 			"sharedSecret" => "secret",
+		// 			"permissions" => "webdav-property",
+		// 		],
+		// 	],
+		// ]);
+		// $this->assertEquals("[]", $output);
+    // $output = curlPost("ocm/ListReceivedShares", []);
+		// $this->assertEquals("[]", $output);
+    // $output = curlPost("ocm/UnShare", []);
+		// $this->assertEquals("[]", $output);
+    // $output = curlPost("ocm/ListReceivedShares", []);
+		// $this->assertEquals("[]", $output);
 	}
 }
