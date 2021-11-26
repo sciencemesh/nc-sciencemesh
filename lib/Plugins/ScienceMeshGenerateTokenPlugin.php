1b1889513fde545ce9a09728e529b6378f3609c8
Ismoil Shifoev <alex.malikov94@gmail.com>
2021-11-26 09:48:29 +0500

    Create a plugin for configure reva REST API call

---
+++ lib/Plugins/ScienceMeshGenerateTokenPlugin.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace OCA\ScienceMesh\Plugins;
+
+use OCP\Collaboration\Collaborators\ISearchPlugin;
+use OCP\Collaboration\Collaborators\ISearchResult;
+use OCP\Collaboration\Collaborators\SearchResultType;
+use OCP\IConfig;
+use OCP\IUserManager;
+use OCP\IUserSession;
+use OCA\ScienceMesh\RevaHttpClient;
+
+class ScienceMeshGenerateTokenPlugin {
+	protected $shareeEnumeration;
+	/** @var IConfig */
+	private $config;
+	/** @var IUserManager */
+	private $userManager;
+	/** @var string */
+	private $userId = '';
+    private $httpClient;
+
+	public function __construct(IConfig $config, IUserManager $userManager, IUserSession $userSession, RevaHttpClient $httpClient) {
+		$this->config = $config;
+		$this->userManager = $userManager;
+		$user = $userSession->getUser();
+		if ($user !== null) {
+			$this->userId = $user->getUID();
+		}
+		$this->shareeEnumeration = $this->config->getAppValue('core', 'shareapi_allow_share_dialog_user_enumeration', 'yes') === 'yes';
+        $this->httpClient = $httpClient;
+	}
+
+    public function generateTokenFromReva() {
+        $request = [
+			'opaque' => [
+				'map' => [
+					'key' => 'test123',
+					'value' => 'test123'
+				]
+			]
+		];
+        $tokenFromReva = $this->httpClient->revaPost('ocm-invite-generate', json_encode($request)); //params will be empty or not fix me
+        return $tokenFromReva;
+    }
+}
