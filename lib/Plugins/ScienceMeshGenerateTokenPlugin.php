26fe396a4f0e237ee77a8e58671b3300d2d1e420
Ismoil Shifoev <alex.malikov94@gmail.com>
2021-11-30 11:18:03 +0500

    Write the test case

--- lib/Plugins/ScienceMeshGenerateTokenPlugin.php
+++ lib/Plugins/ScienceMeshGenerateTokenPlugin.php
@@ -6,7 +6,6 @@ use OCP\IConfig;
 use OCP\IUserManager;
 use OCP\IUserSession;
 use OCA\ScienceMesh\RevaHttpClient;
-use OCP\Security\ISecureRandom;
 use OCA\ScienceMesh\User\ScienceMeshUserId;
 
 class ScienceMeshGenerateTokenPlugin {
@@ -18,44 +17,35 @@ class ScienceMeshGenerateTokenPlugin {
 	/** @var string */
 	private $userId = '';
 	private $httpClient;
-	private $secureRandom;
 
-	public function __construct(IConfig $config, IUserManager $userManager, IUserSession $userSession, RevaHttpClient $httpClient, ISecureRandom $secureRandom) {
+	public function __construct(IConfig $config, IUserManager $userManager, IUserSession $userSession, RevaHttpClient $httpClient) {
 		$this->config = $config;
 		$this->userManager = $userManager;
 		$user = $userSession->getUser();
-		if ($user !== null) {
-			$this->userId = $user->getUID();
-		}
+	
 		$this->shareeEnumeration = $this->config->getAppValue('core', 'shareapi_allow_share_dialog_user_enumeration', 'yes') === 'yes';
 		$this->httpClient = $httpClient;
-		$this->secureRandom = $secureRandom;
 	}
 
 	public function getGenerateTokenResponse() {
-		//$result = $this->generateTokenFromReva();// Configure if the reva endpoint will be ready
-		if ($this->userManager->userExists($this->userId)) {
-			$invitationsData = [
-				"status" => [
-					"code" => 1,
-					"trace" => "00000000000000000000000000000000"
+		$invitationsData = [
+			"status" => [
+				"code" => 1,
+				"trace" => "00000000000000000000000000000000"
+			],
+			"invite_token" => [
+				"token" => "161405dd-05f0-4806-8b42-1482b3185c63",
+				"user_id" => [
+					"idp" => "some-idp",
+					"opaque_id" => "einstein",
+					"type" => ScienceMeshUserId::USER_TYPE_PRIMARY
 				],
-				"invite_token" => [
-					"token" => "161405dd-05f0-4806-8b42-1482b3185c63",
-					"user_id" => [
-						"idp" => "some-idp",
-						"opaque_id" => $this->userId,
-						"type" => ScienceMeshUserId::USER_TYPE_PRIMARY
-					],
-					"expiration" => [
-						"seconds" => gmdate("H:i:s", 1638025012)
-					]
+				"expiration" => [
+					"seconds" => gmdate("H:i:s", 1638025012)
 				]
-			];
-			return $invitationsData;
-		}
-		
-		return new JSONResponse("Username / password not recognized", 401);
+			]
+		];
+		return $invitationsData;
 	}
 
 	public function generateTokenFromReva() {
