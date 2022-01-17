ce02b5c3c1e971e36f0aad7280c076b0f83d8995
Michiel de Jong <michiel@unhosted.org>
2022-01-17 14:22:44 +0100

    Fix contacts template

--- templates/contacts.php
+++ templates/contacts.php
@@ -8,34 +8,12 @@ style('sciencemesh', 'style');
 	</div>
 	<div id="app-content">
 		<div id="app-content-wrapper" class="viewcontainer">
-				<div class="app-content-list">
-					<div href="#" class="app-content-list-item" id="test">
-						<!-- div class="app-content-list-item-star icon-starred"></div -->
-						<div class="app-content-list-item-icon" style="background-color: rgb(151, 72, 96);">M</div>
-						<div class="app-content-list-item-line-one" id="show_result"></div>
-						<div class="icon-delete"></div>
-					</div>
-					<div href="#" class="app-content-list-item">
-						<!-- div class="app-content-list-item-star icon-starred"></div -->
-						<div class="app-content-list-item-icon" style="background-color: rgb(31, 72, 96);">+</div>
-						<!-- div class="app-content-list-item-line-one">Accept an invitation</div -->
-						<div class="app-content-list-item-line-one" >
-							<input id="token" type="text" placeholder="Enter your token here">
-						</div>
-						<div class="app-content-list-item-menu">
-						 	<div class="icon-add" id="elem"></div>
-						</div>
-					</div>
+			<div class="app-content-list">
+				<div href="#" class="app-content-list-item" id="test">
+					<div class="app-content-list-item-icon" style="background-color: rgb(151, 72, 96);">M</div>
+					<div class="app-content-list-item-line-one" id="show_result"></div>
 				</div>
-				<div class="app-content-detail">
-					<div class="section">
-						<!--<p>To start collaborating with someone on ScienceMesh, enter the received invitation token here.</p>
-						<p>To send an invitation, generate a token in 'Invitations' and send it to them.</p>-->
-						<p id="test_error"></p>
-						<p id="display_name"></p>
-						<p id="provider"></p>
-					</div>
-				</div>
+			</div>
 		</div>
 	</div>
 </div>
