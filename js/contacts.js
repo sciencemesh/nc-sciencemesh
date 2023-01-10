cb2ea4043f8754a6fecc64dafc3b82e637803682
Michiel de Jong <michiel@unhosted.org>
2023-01-10 10:20:35 +0100

    Frontend modification (#228)
    * alert success
    
    * ui modification
    
    * style.css
    
    * ui final
    
    * PHP unit test fail fix
    
    * fix https://github.com/pondersource/nc-sciencemesh/issues/229
    
    Co-authored-by: parham_above <parham@above.ge>
    Co-authored-by: Parham R <parhamin2010@gmail.com>

--- js/contacts.js
+++ js/contacts.js
@@ -1,33 +1,55 @@
 //Everything will be for working with contacts
 var baseUrl = OC.generateUrl('/apps/sciencemesh');
-$('#test').hide(); 
+$('#test_error').hide(); 
 $.ajax({
     url: baseUrl + '/contacts/users',
     type: 'GET',
     contentType: 'application/json',
 }).done(function (response) {
-    if(response === '' || response === false) {
+    if(response == '' || response === false) {
         var element = document.getElementById("test_error");
         element.innerHTML= 'No connection with reva';
-        //$('#test').show(); 
+        $('#test_error').show(); 
     } else {
     let token = JSON.parse(response);
+
     for(tokenData in token) {
         if(token.hasOwnProperty(tokenData)) {
+            console.log(tokenData);
             if(tokenData === 'accepted_users') {
                 let accepted_users = token.accepted_users
-                console.log('Accepted users', accepted_users)
-                for(accept in accepted_users) {
-                    const displayName = accepted_users[accept].display_name;
-                    const username = accepted_users[accept].id.opaque_id;
-                    const idp = accepted_users[accept].id.idp;
-                    const provider = new URL(idp).host;
-                    const result = `${displayName} (${username}@${provider})`;
-                  
-                    var element = document.getElementById("show_result");
-                    element.innerHTML=result;
-                    $('#test').show();
-                }
+                    for(accept in accepted_users) {
+                        const displayName = accepted_users[accept].display_name;
+                        const username = accepted_users[accept].id.opaque_id;
+                        const idp = accepted_users[accept].id.idp;
+                        const provider = new URL(idp).host;
+                        const result = `
+                                <div href="#" class="app-content-list-item profile-item">
+                                    <div class="app-content-list-item-icon" style="">
+                                        <img src="https://cdn-icons-png.flaticon.com/512/16/16363.png">
+                                    </div>
+                                    <div class="app-content-list-item-line-one" id="show_result" >
+                                        <p class="displayname">${displayName}</p><p class="username-provider">${username}@${provider}</p>
+                                    </div>  
+                                </div>`;                  
+                        var element = document.getElementById("test_error");
+                        element.innerHTML = result;
+                    }
+
+                $('#test_error').show();
+            }else{
+                const result = `
+                        <div href="#" class="app-content-list-item profile-item" >
+                            <div class="app-content-list-item-icon" style="">
+                            </div> 
+                            <div class="app-content-list-item-line-one" id="show_result" >
+                                <p class="username-provider">There're no contacts!</p>
+                            </div>  
+                        </div>`;                  
+                var element = document.getElementById("test_error");
+                element.innerHTML = result;
+                $('#test_error').show();
+
             }
         } 
     }
