5615f77d1e8b8ca321d3d196f4639e86baacee38
parham R <parhamin2010@gmail.com>
2023-01-10 13:28:19 +0330

    fix loop back text-shared secret editable

--- templates/settings/admin.php
+++ templates/settings/admin.php
@@ -21,10 +21,10 @@ style('sciencemesh', 'style');
         </div>
         <div class="sciencemesh-settings-row">
             <label for="sciencemesh_shared_secret" name="sciencemesh_shared_secret">Shared Secret</label>
-            <input type="text" readonly="true" id="sciencemesh_shared_secret" value="<?php echo $this->vars['sciencemeshRevaSharedSecret']; ?>">
+            <input type="text" name="sciencemesh_shared_secret" id="sciencemesh_shared_secret" value="<?php echo $this->vars['sciencemeshRevaSharedSecret']; ?>">
         </div>
         <div class="sciencemesh-settings-row">
-            <label for="sciencemesh_loopback_shared_secret" name="sciencemesh_iop_url">Loopback Shared Secret</label>
+            <label for="sciencemesh_loopback_shared_secret" name="sciencemesh_iop_url">Loopback Secret</label>
             <input type="text" readonly="true" id="sciencemesh_loopback_shared_secret" value="<?php echo $this->vars['sciencemeshRevaLoopbackSecret']; ?>">
         <div>
             <input type="button" name="sciencemesh_setting_submit_btn" id="sciencemesh_setting_submit_btn" value="Save settings">
