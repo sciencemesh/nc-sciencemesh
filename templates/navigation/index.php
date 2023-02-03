c7b9d592807488af637e81f155c52c6061e39d18
Michiel de Jong <michiel@unhosted.org>
2023-02-03 15:16:32 +0000

    Make contacts the default tab

--- templates/navigation/index.php
+++ templates/navigation/index.php
@@ -1,10 +1,4 @@
 <ul class="with-icon">
-    <li>
-        <a class="icon-public" style="padding-left:34px" href="generate">Create invite link
-            <button id="token-generator" class="icon-add-white app-content-list-item-icon" style="background-color: rgb(31, 72, 96);">
-    </button>
-</a>
-    </li>
     <!-- <li><a class="icon-mail" style="padding-left:34px" href="accept">Accept ScienceMesh Invite</a></li> -->
     <li><a class="icon-contacts-dark" style="padding-left:34px" href="contacts">ScienceMesh Contacts</a></li>
 </ul>
