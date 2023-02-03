db530376d05697b2eaab92a800e9be4862bd58fd
Michiel de Jong <michiel@unhosted.org>
2023-02-03 14:56:15 +0000

    Generate button in the navigation

--- templates/navigation/index.php
+++ templates/navigation/index.php
@@ -1,8 +1,11 @@
 <ul class="with-icon">
     <li>
         <a class="icon-public" style="padding-left:34px" href="generate">Create invite link
-        <button><span class="icon-add"></span></button></a>
+            <button class="token-generator icon-add-white app-content-list-item-icon" style="background-color: rgb(31, 72, 96);">
+        <!-- <span class="icon-add-white"></span> -->
+    </button>
+</a>
     </li>
-    <li><a class="icon-mail" style="padding-left:34px" href="accept">Accept ScienceMesh Invite</a></li>
+    <!-- <li><a class="icon-mail" style="padding-left:34px" href="accept">Accept ScienceMesh Invite</a></li> -->
     <li><a class="icon-contacts-dark" style="padding-left:34px" href="contacts">ScienceMesh Contacts</a></li>
 </ul>
