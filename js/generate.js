0a2193bedc82341cb4c607ad68ad87b46c90eab3
parham R <parhamin2010@gmail.com>
2023-02-15 16:20:51 +0330

    remove console log

--- js/generate.js
+++ js/generate.js
@@ -2,7 +2,6 @@
 //give some error you need for every PHP file seperate JS logic
 document.addEventListener("DOMContentLoaded", function(event) {
     $('#test').hide();
-    console.log("Hello there");
     console.log(document.getElementById('token-generator'));
     document.getElementById('token-generator').onclick = function () {
         var baseUrl = OC.generateUrl('/apps/sciencemesh');
