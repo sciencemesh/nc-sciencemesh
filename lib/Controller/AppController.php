c4d4dd4841ac36484958ae42e2d44b546986221f
Michiel de Jong <michiel@unhosted.org>
2023-04-27 16:45:07 +0000

    Http::STATUS_INTERNAL_SERVER_ERROR

--- lib/Controller/AppController.php
+++ lib/Controller/AppController.php
@@ -123,10 +123,10 @@ class AppController extends Controller {
 		// $meshDirectoryUrl = "https://sciencemesh.cesnet.cz/iop/meshdir/";
 		$meshDirectoryUrl = $this->config->getAppValue('sciencemesh', 'meshDirectoryUrl', 'https://sciencemesh.cesnet.cz/iop/meshdir/');
     if (!$inviteLinkStr) {
-			return new TextPlainResponse("Unexpected response from Reva", Http::STATUS_INTERNAL_ERROR);
+			return new TextPlainResponse("Unexpected response from Reva", Http::STATUS_INTERNAL_SERVER_ERROR);
 		}
     if (!$meshDirectoryUrl) {
-			return new TextPlainResponse("Unexpected mesh directory URL configuration", Http::STATUS_INTERNAL_ERROR);
+			return new TextPlainResponse("Unexpected mesh directory URL configuration", Http::STATUS_INTERNAL_SERVER_ERROR);
 		}
 
 		return new TextPlainResponse("$meshDirectoryUrl$inviteLinkStr", Http::STATUS_OK);
