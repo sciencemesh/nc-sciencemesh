fa8a5aeac3cba33311bd4cd63ce12e71a434d972
Michiel de Jong <michiel@unhosted.org>
2023-05-10 15:19:00 +0200

    Running release.sh inside container

--- release.sh
+++ release.sh
@@ -1,3 +1,8 @@
+echo run this file as www-data user inside a Docker container
+echo first, docker exec as root and chown /var/www/html/apps/sciencemesh to www-user
+echo then, docker exec as www-user, and store sciencemesh.key into /var/www/sciencemesh.key (make sure to remove it or delete the container after you're done)
+echo then cd into apps/sciencemesh, and run this script.
+
 mkdir -p build/sciencemesh
 rm -rf build/sciencemesh/*
 cp -r appinfo build/sciencemesh/
@@ -9,17 +14,12 @@ cp -r templates build/sciencemesh/
 cp -r composer.* build/sciencemesh/
 cd build/sciencemesh/
 composer install
-cd ..
+cd /var/www/html
+./occ integrity:sign-app --privateKey=/var/www/sciencemesh.key --certificate=apps/sciencemesh/sciencemesh.crt --path=apps/sciencemesh/build/sciencemesh
+cd apps/sciencemesh/build
 tar -cf sciencemesh.tar sciencemesh
 cd ../release
 mv ../build/sciencemesh.tar .
 rm -f -- sciencemesh.tar.gz
 gzip sciencemesh.tar
 cd ..
-echo copy sciencemesh.key into an oc1 container and run:
-echo docker exec -it oc1.docker bash
-echo -> chown -R www-data apps/sciencemesh
-echo -> exit
-echo docker exec -it -u www-data oc1.docker bash
-echo -> ./occ integrity:sign-app --privateKey=/var/www/sciencemesh.key --certificate=apps/sciencemesh/sciencemesh.crt --path=apps/sciencemesh
-echo now commit the appinfo/signature.json file inside apps/sciencemesh
