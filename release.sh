66e67a304ef4e53ff56bcfeb1df1b7521b6e4eb9
Michiel de Jong <michiel@unhosted.org>
2023-05-11 19:56:53 +0200

    Update release.sh

--- release.sh
+++ release.sh
@@ -16,3 +16,9 @@ mv ../build/sciencemesh.tar .
 rm -f -- sciencemesh.tar.gz
 gzip sciencemesh.tar
 cd ..
+echo `openssl dgst -sha512 -sign ~/sciencemesh.key ./sciencemesh.tar.gz | openssl base64`
+echo visit https://apps.nextcloud.com/developer/apps/releases/new
+echo go into the developer tools browser console to change the `<a>` element around the form to a `<p>` element. This makes it possible to fill in values in this form without being redirected.
+echo fill in for instance `https://github.com/pondersource/nc-sciencemesh/raw/sciencemesh/release/sciencemesh.tar.gz` and the base64 signature from the openssl command
+echo click 'uploaden'
+echo good luck!
