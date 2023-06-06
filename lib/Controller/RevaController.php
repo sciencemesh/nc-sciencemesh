658c2e1126bb5324ef9359a983cfbca42436db6c
Michiel de Jong <michiel@unhosted.org>
2023-06-06 15:42:04 +0200

    GetSentShareByToken in RevaController.php

--- lib/Controller/RevaController.php
+++ lib/Controller/RevaController.php
@@ -1124,4 +1124,22 @@ class RevaController extends Controller {
 		}
 		return new JSONResponse(["error" => "GetSentShare failed"], Http::STATUS_BAD_REQUEST);
 	}
+	
+	/**
+	 * @PublicPage
+	 * @NoCSRFRequired
+	 * @return Http\DataResponse|JSONResponse
+	 *
+	 * GetSentShareByToken gets the information for a share by the given token.
+	 */
+	public function GetSentShareByToken() {
+		error_log("GetSentShareByToken");
+		$token = $this->request->getParam("Spec")["Token"];
+		$share = $this->shareProvider->getShareByToken($token);
+		if ($share) {
+			$response = $this->shareInfoToCs3Share($share);
+			return new JSONResponse($response, Http::STATUS_OK);
+		}
+		return new JSONResponse(["error" => "GetSentShare failed"], Http::STATUS_BAD_REQUEST);
+	}
 }
