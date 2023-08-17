d606369f889500e90924a4401c50b1dff859ebda
Mohammad Mahdi Baghbani Pourvahid <Mahdi.baghbani1@gmail.com>
2023-08-17 16:11:45 +0000

    remove: white lines

--- lib/AppInfo/ScienceMeshApp.php
+++ lib/AppInfo/ScienceMeshApp.php
@@ -32,9 +32,6 @@ class ScienceMeshApp extends App {
 			return $c->query('UserSession')->getUser();
 		});
 
-
-
-
 		$notificationManager = $server->getNotificationManager();
         $notificationManager->registerNotifier(function () use ($notificationManager) {
             return $this->getContainer()->query('\OCA\ScienceMesh\Notifier\ScienceMeshNotifier');
